b6991957b981d7e1917bbf72a010ec1dae17323a
bharath-reddy07 <97978349+bharath-reddy07@users.noreply.github.com>
2023-01-20 14:39:44 +0530

    api key

--- src/addFood.js
+++ src/addFood.js
@@ -1,6 +1,6 @@
 var image = document.getElementById("image")
 var submit = document.getElementById("submit")
-var apiKey = "71f206c0c36613debc964d5930a1ee70a50c62c6"
+var apiKey = "90e1ba7815ee31c248cbfcf44bfe357ca8757b4e"
 var myHeaders = new Headers();
 
 var formdata = new FormData();
